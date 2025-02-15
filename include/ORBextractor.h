b6d5ea63666291b0c359fa7f9245a108439eee57
zhangrifeng <zhangrifeng2000@163.com>
2025-02-15 21:19:14 +0800

    Update src and include

--- include/ORBextractor.h
+++ include/ORBextractor.h
@@ -21,7 +21,8 @@
 
 #include <vector>
 #include <list>
-#include <opencv/cv.h>
+// #include <opencv/cv.h>
+#include <opencv2/opencv.hpp>
 
 
 namespace ORB_SLAM3
--- src/Optimizer.cc
+++ src/Optimizer.cc
@@ -3997,10 +3997,10 @@ int Optimizer::OptimizeSim3(KeyFrame *pKF1, KeyFrame *pKF2, vector<MapPoint *> &
     int nOutKF2 = 0;
     int nMatchWithoutMP = 0;
 
-    cv::Mat img1 = cv::imread(pKF1->mNameFile, CV_LOAD_IMAGE_UNCHANGED);
-    cv::cvtColor(img1, img1, CV_GRAY2BGR);
-    cv::Mat img2 = cv::imread(pKF2->mNameFile, CV_LOAD_IMAGE_UNCHANGED);
-    cv::cvtColor(img2, img2, CV_GRAY2BGR);
+    cv::Mat img1 = cv::imread(pKF1->mNameFile, cv::IMREAD_UNCHANGED);
+    cv::cvtColor(img1, img1, cv::COLOR_GRAY2BGR);
+    cv::Mat img2 = cv::imread(pKF2->mNameFile, cv::IMREAD_UNCHANGED);
+    cv::cvtColor(img2, img2, cv::COLOR_GRAY2BGR);
 
     vector<int> vIdsOnlyInKF2;
 
@@ -6792,9 +6792,9 @@ void Optimizer::LocalBundleAdjustment(KeyFrame* pMainKF,vector<KeyFrame*> vpAdju
             if(bShowImages)
             {
                 string strNameFile = pKFi->mNameFile;
-                cv::Mat imLeft = cv::imread(strNameFile, CV_LOAD_IMAGE_UNCHANGED);
+                cv::Mat imLeft = cv::imread(strNameFile, cv::IMREAD_UNCHANGED);
 
-                cv::cvtColor(imLeft, imLeft, CV_GRAY2BGR);
+                cv::cvtColor(imLeft, imLeft, cv::COLOR_GRAY2BGR);
 
                 int numPointsMono = 0, numPointsStereo = 0;
                 int numPointsMonoBad = 0, numPointsStereoBad = 0;
